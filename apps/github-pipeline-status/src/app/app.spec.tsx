45c042b0bd757c0dda51c9e0c89a6cf3fad6309b
Martin Gansler <martin.gansler@codecentric.de>
2021-02-26 15:27:56 +0100

    Add basic styling for body and header

--- apps/github-pipeline-status/src/app/app.spec.tsx
+++ apps/github-pipeline-status/src/app/app.spec.tsx
@@ -21,6 +21,6 @@ describe('App', () => {
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />)
 
-    expect(getByText('Welcome to github-pipeline-status!')).toBeTruthy()
+    expect(getByText(/github pipeline status/i)).toBeTruthy()
   })
 })
