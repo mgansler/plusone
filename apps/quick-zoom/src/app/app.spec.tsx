e29477d2177112ae1e4f5b311b8e07c445431d80
Martin Gansler <martin.gansler@codecentric.de>
2021-02-28 22:45:28 +0100

    Add quick-zoom to workspace (#54)

---
+++ apps/quick-zoom/src/app/app.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import App from './app'
+
+describe('App', () => {
+  it('should have a greeting as the title', () => {
+    render(<App />)
+
+    expect(screen.getByText('Welcome to quick-zoom!')).toBeInTheDocument()
+  })
+})
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   projects: [
     '<rootDir>/apps/github-pipeline-status',
+    '<rootDir>/apps/quick-zoom',
     '<rootDir>/libs/github-schema',
     '<rootDir>/libs/hooks',
     '<rootDir>/libs/input',
