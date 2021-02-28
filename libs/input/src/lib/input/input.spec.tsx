6dc08cbcc84dada7e4a85fc15693b6ca73a3272a
Martin Gansler <martin.gansler@codecentric.de>
2021-02-28 16:07:43 +0100

    Resolves #46: Add description to login page

--- libs/input/src/lib/input/input.spec.tsx
+++ libs/input/src/lib/input/input.spec.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Input } from './input'
 
 describe('Input', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Input label={'Label'} />)
-    expect(baseElement).toBeTruthy()
+  it('should render a label', () => {
+    render(<Input label={'Label'} />)
+
+    expect(screen.getByText(/label/i)).toBeInTheDocument()
+  })
+
+  it('should allow access via ref', () => {
+    const inputRef = React.createRef<HTMLInputElement>()
+
+    render(<Input label={'Label'} inputRef={inputRef} />)
+
+    userEvent.type(screen.getByLabelText(/label/i), 'Hello World')
+
+    expect(inputRef.current.value).toBe('Hello World')
   })
 })
