ccfff0b30c65c7d3501dadbb05ce7bf234bbf78a
Martin Gansler <martin.gansler@codecentric.de>
2021-03-02 07:13:53 +0100

    Add @material-ui/core and @material-ui/icons

--- apps/github-pipeline-status/src/main.tsx
+++ apps/github-pipeline-status/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
 
 import { App } from './app/app'
 import { OctokitProvider } from './app/octokit-provider/octokit-provider'
@@ -12,6 +13,7 @@ const queryClient = new QueryClient({
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <QueryClientProvider client={queryClient}>
       <OctokitProvider>
         <BrowserRouter>
--- apps/quick-zoom/src/main.tsx
+++ apps/quick-zoom/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { CssBaseline } from '@material-ui/core'
 
 import App from './app/app'
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
