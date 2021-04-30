c091cfba6d85b68aa723d4d97ac8b35b27f12d3a
Martin Gansler <martin.gansler@codecentric.de>
2021-04-30 08:41:10 +0200

    Add listener for 'discover' queue

--- apps/discovery/src/main.ts
+++ apps/discovery/src/main.ts
@@ -1,1 +1,27 @@
-console.log('Hello World!')
+import { connect } from 'amqplib/callback_api'
+
+connect('amqp://localhost', (connectErr, connection) => {
+  if (connectErr) {
+    throw connectErr
+  }
+
+  connection.createChannel((channelErr, channel) => {
+    if (channelErr) {
+      throw channelErr
+    }
+
+    const queue = 'discover'
+
+    channel.assertQueue(queue, { durable: false })
+
+    console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue)
+
+    channel.consume(
+      queue,
+      (msg) => {
+        console.log(' [x] Received %s', msg.content.toString())
+      },
+      { noAck: true },
+    )
+  })
+})
