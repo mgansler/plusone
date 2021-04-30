4b2df3d07f93fe51aa3b95a2d7f7e29c9da8d07f
Martin Gansler <martin.gansler@codecentric.de>
2021-04-30 09:54:01 +0200

    Connect controller app to rabbitmq

--- apps/controller/src/app/app.module.ts
+++ apps/controller/src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { ClientsModule, Transport } from '@nestjs/microservices'
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'DISCOVER_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://localhost'],
+          queue: 'discover',
+          queueOptions: { durable: false },
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/controller/src/app/app.service.ts
+++ apps/controller/src/app/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common'
+import { Inject, Injectable } from '@nestjs/common'
+import { ClientProxy } from '@nestjs/microservices'
 
 @Injectable()
 export class AppService {
+  constructor(@Inject('DISCOVER_SERVICE') private discoverClient: ClientProxy) {}
+
   addWebsite({ uri }): { uri: string } {
+    this.discoverClient.emit('website', { uri })
     return { uri }
   }
 }
