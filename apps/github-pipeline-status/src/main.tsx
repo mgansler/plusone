2e2c169c031abe3577c0e53bff626af1c7ac0d46
Martin Gansler <martin.gansler@codecentric.de>
2021-06-01 17:38:55 +0200

    Use dark mode when user prefers it

--- apps/github-pipeline-status/src/main.tsx
+++ apps/github-pipeline-status/src/main.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter } from 'react-router-dom'
-import { createMuiTheme, CssBaseline, ThemeProvider } from '@material-ui/core'
+import { createMuiTheme, CssBaseline, ThemeProvider, useMediaQuery } from '@material-ui/core'
+import React, { useMemo } from 'react'
 
 import { App } from './app/app'
 import { OctokitProvider } from './app/octokit-provider/octokit-provider'
@@ -10,18 +11,25 @@ const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 0, refetchOnWindowFocus: false } },
 })
 
-const theme = createMuiTheme({})
+function ThemedApp() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+  const theme = useMemo(
+    () => createMuiTheme({ palette: { type: prefersDarkMode ? 'dark' : 'light' } }),
+    [prefersDarkMode],
+  )
 
-ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <CssBaseline />
+  return (
     <ThemeProvider theme={theme}>
-      <OctokitProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </OctokitProvider>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <OctokitProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </OctokitProvider>
+      </QueryClientProvider>
     </ThemeProvider>
-  </QueryClientProvider>,
-  document.getElementById('root'),
-)
+  )
+}
+
+ReactDOM.render(<ThemedApp />, document.getElementById('root'))
