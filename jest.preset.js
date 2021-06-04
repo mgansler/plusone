55f55bee5dabcf971a032aaec79af4a8724db652
Martin Gansler <martin@martingansler.de>
2021-06-04 09:02:50 +0000

    Test reports

--- jest.preset.js
+++ jest.preset.js
@@ -3,4 +3,14 @@ const nxPreset = require('@nrwl/jest/preset')
 module.exports = {
   ...nxPreset,
   testEnvironment: 'jsdom',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports/test',
+        outputName: `test-${process.env.NX_TASK_HASH}.xml`,
+      },
+    ],
+  ],
 }
