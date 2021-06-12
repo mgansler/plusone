590118a9a1931c6be4cce1a6bac1d79306dbc922
Martin Gansler <martin.gansler@codecentric.de>
2021-06-12 11:56:09 +0200

    Automatically create an admin user

--- libs/feeds/backend/user/src/lib/user.service.ts
+++ libs/feeds/backend/user/src/lib/user.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@nestjs/common'
-import { Repository } from 'typeorm'
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { hash } from 'bcrypt'
 
 import { User } from '@plusone/feeds/backend/database'
 
 import { UserRegisterDto } from './user-register.dto'
 
 @Injectable()
-export class UserService {
+export class UserService implements OnModuleInit {
+  private readonly logger = new Logger(UserService.name)
+
   constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+  async onModuleInit(): Promise<void> {
+    await this.createRootUser()
+  }
 
   async create(user: UserRegisterDto): Promise<User> {
     const createdUser = await this.userRepository.create(user)
@@ -17,5 +24,19 @@ export class UserService {
 
   async findOne(username: string): Promise<User | undefined> {
     return this.userRepository.findOne({ where: { username } })
+  }
+
+  private async createRootUser() {
+    const rootUsername = process.env.ADMIN_USER
+    const rootPassword = process.env.ADMIN_PASSWORD
+    if (!(await this.userRepository.findOne({ where: { username: rootUsername } }))) {
+      this.logger.log(`Admin user does not exist, creating account '${rootUsername}'`)
+      const rootUser = await this.userRepository.create({
+        username: rootUsername,
+        password: await hash(rootPassword, 10),
+        isAdmin: true,
+      })
+      await this.userRepository.save(rootUser)
+    }
   }
 }
