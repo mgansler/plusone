651302a9c633f584f0349acaa2af7afbc881288f
Martin Gansler <martin.gansler@codecentric.de>
2021-06-18 07:04:30 +0200

    Add comments

--- tools/scripts/format-staged-files.ts
+++ tools/scripts/format-staged-files.ts
@@ -1,13 +1,15 @@
 import { execSync } from 'child_process'
 
-// Get changed files
+// Get currently staged files
 const stagedFiles: string[] = execSync('git diff --name-only --cached').toString().split(/\n/).filter(Boolean)
 
 // Format files
 execSync(`yarn nx format --files ${stagedFiles.join(',')}`)
 
+// Get unstaged files after commit
 const unstagedFiles: string[] = execSync('git diff --name-only').toString().split(/\n/).filter(Boolean)
 
+// Stage files that have been staged before again
 for (const stagedFile of stagedFiles) {
   if (unstagedFiles.includes(stagedFile)) {
     execSync(`git add ${stagedFile}`)
