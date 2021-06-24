b0e639f317b4c36b432fcfccfd981c76522f7023
Martin Gansler <martin.gansler@codecentric.de>
2021-06-24 09:31:13 +0200

    Setup test correctly

--- libs/feeds/web/user-view/src/lib/feeds-web-user-view.spec.tsx
+++ libs/feeds/web/user-view/src/lib/feeds-web-user-view.spec.tsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+import { MockAuthenticationProvider } from '@plusone/feeds/web/login'
 
 import { FeedsWebUserView } from './feeds-web-user-view'
 
 describe('FeedsWebUserView', () => {
+  const testQueryClient = new QueryClient({ defaultOptions: { queries: { retry: false } } })
   it('should render successfully', () => {
-    const { baseElement } = render(<FeedsWebUserView />)
+    const { baseElement } = render(
+      <MockAuthenticationProvider>
+        <QueryClientProvider client={testQueryClient}>
+          <FeedsWebUserView />
+        </QueryClientProvider>
+      </MockAuthenticationProvider>,
+    )
     expect(baseElement).toBeTruthy()
   })
 })
