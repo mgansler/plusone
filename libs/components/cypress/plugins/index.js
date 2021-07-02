69f0d919a8d9ade15b679cfe9bd40c2e280822ea
Martin Gansler <martin.gansler@codecentric.de>
2021-07-02 07:25:34 +0200

    Use webpack config from @nrwl/cypress

--- libs/components/cypress/plugins/index.js
+++ libs/components/cypress/plugins/index.js
@@ -1,8 +1,17 @@
-const { preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor')
+const { join } = require('path')
+
+const { startDevServer } = require('@cypress/webpack-dev-server')
+const { getWebpackConfig, preprocessTypescript } = require('@nrwl/cypress/plugins/preprocessor')
 
 module.exports = (on, config) => {
   on('file:preprocessor', preprocessTypescript(config))
 
-  require('@cypress/react/plugins/babel')(on, config)
-  return config
+  // For when we run `npx cypress open-ct -C cypress.json` from the components directory
+  if (config.env.tsConfig.startsWith('.')) {
+    config.env.tsConfig = join(__dirname, '../..', config.env.tsConfig)
+  }
+
+  const webpackConfig = getWebpackConfig(config)
+
+  on('dev-server:start', (options) => startDevServer({ options, webpackConfig }))
 }
