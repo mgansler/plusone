2b344494130d53ffe417916bb7cd0ebd2a2500fb
Martin Gansler <martin.gansler@codecentric.de>
2021-07-07 11:52:30 +0200

    Add test for pasting in the website url

--- libs/feeds/web/new-feed/src/lib/feeds-web-new-feed.ct.tsx
+++ libs/feeds/web/new-feed/src/lib/feeds-web-new-feed.ct.tsx
@@ -7,11 +7,13 @@ import { MockAuthenticationProvider } from '../../../login/src'
 import { FeedsWebNewFeed } from './feeds-web-new-feed'
 
 describe('FeedsWebNewFeed', () => {
-  it('should fill inputs with response from discovery', () => {
+  beforeEach(() => {
     cy.intercept('GET', `/api/feed/discover?url=${encodeURIComponent(discoverFeed.url)}`, (req) => {
       req.reply(discoverFeed)
     })
+  })
 
+  it('should fill inputs with response from discovery when typing', () => {
     mount(
       <MockAuthenticationProvider>
         <QueryClientProvider client={new QueryClient()}>
@@ -21,6 +23,26 @@ describe('FeedsWebNewFeed', () => {
     )
 
     cy.findByRole('textbox', { name: /website/i }).type(discoverFeed.url)
+
+    cy.findByRole('textbox', { name: /title/i }).should('have.value', discoverFeed.title)
+    cy.findByRole('textbox', { name: /feed url/i }).should('have.value', discoverFeed.feedUrl)
+
+    cy.findByRole('button', { name: /add feed/i }).click()
+  })
+
+  it('should fill inputs with response from discovery when pasting', () => {
+    mount(
+      <MockAuthenticationProvider>
+        <QueryClientProvider client={new QueryClient()}>
+          <FeedsWebNewFeed />
+        </QueryClientProvider>
+      </MockAuthenticationProvider>,
+    )
+
+    cy.findByRole('textbox', { name: /website/i })
+      .focus()
+      .invoke('val', discoverFeed.url)
+      .blur()
 
     cy.findByRole('textbox', { name: /title/i }).should('have.value', discoverFeed.title)
     cy.findByRole('textbox', { name: /feed url/i }).should('have.value', discoverFeed.feedUrl)
