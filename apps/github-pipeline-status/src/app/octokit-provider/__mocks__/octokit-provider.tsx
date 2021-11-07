ee003a00a66687094ed06205f7f31ca257482518
Martin Gansler <martin.gansler@codecentric.de>
2021-11-07 14:27:58 +0100

    Remove no longer used auto mocks

--- apps/github-pipeline-status/src/app/octokit-provider/__mocks__/octokit-provider.tsx
+++
@@ -1,5 +0,0 @@
-import { Octokit } from '@octokit/core'
-
-export const useOctokit = () => new Octokit()
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export const useLogout = () => {}
--- apps/github-pipeline-status/src/app/organizations/__mocks__/organizations.tsx
+++
@@ -1,1 +0,0 @@
-export const Organizations = () => <div>Organizations</div>
