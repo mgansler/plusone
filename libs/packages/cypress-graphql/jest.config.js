76dbb0e8ee90bbf7b72c6e72bbfdf4c8498a3780
Martin Gansler <martin.gansler@codecentric.de>
2021-11-08 10:32:55 +0100

    Init @mgansler/cypress-graphql

---
+++ libs/packages/cypress-graphql/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'packages-cypress-graphql',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/libs/packages/cypress-graphql',
+}
---
+++ libs/packages/cypress-graphql/src/lib/cypress-graphql.ts
@@ -0,0 +1,3 @@
+export function cypressGraphql(): string {
+  return 'packages-cypress-graphql'
+}
