022c23a1a0c77f51476c39fa3fb3396a20cca030
Martin Gansler <martin.gansler@codecentric.de>
2021-11-22 17:27:42 +0100

    Remove custom generator

--- tools/generators/split-workspace/index.ts
+++
@@ -1,9 +0,0 @@
-import { addProjectConfiguration, removeProjectConfiguration, Tree } from '@nrwl/devkit'
-import { readWorkspaceJson } from '@nrwl/workspace'
-
-export default async function (tree: Tree) {
-  Object.entries(readWorkspaceJson().projects).forEach(([name, config]) => {
-    removeProjectConfiguration(tree, name)
-    addProjectConfiguration(tree, name, config, true)
-  })
-}
