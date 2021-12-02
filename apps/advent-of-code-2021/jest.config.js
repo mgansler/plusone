aa3d6448b5cb45fabe6378c5a1bc619610f5ec0b
Martin Gansler <martin.gansler@codecentric.de>
2021-12-02 18:15:40 +0100

    Rename my-remix-app => advent-of-code-2021

--- apps/advent-of-code-2021/jest.config.js
+++ apps/advent-of-code-2021/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'my-remix-app',
+  displayName: 'advent-of-code-2021',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/my-remix-app',
+  coverageDirectory: '../../coverage/apps/advent-of-code-2021',
 }
