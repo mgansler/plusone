bef708c517aa20b4a7237ab43c41911cf787ab4d
Martin Gansler <martin.gansler@codecentric.de>
2021-12-10 19:42:36 +0100

    Day 10 - Part 1

--- apps/advent-of-code-2021/app/routes/days/10.tsx
+++ apps/advent-of-code-2021/app/routes/days/10.tsx
@@ -1,14 +1,43 @@
 import { ActionFunction, Form, json, useActionData } from 'remix'
 
+type Opening = '[' | '{' | '(' | '<'
+type Closing = ']' | '}' | ')' | '>'
+const opening: Opening[] = ['[', '{', '(', '<']
+const closing: Closing[] = [']', '}', ')', '>']
+type Token = Opening | Closing
+
+const points: Record<Closing, number> = { ')': 3, '>': 25137, ']': 57, '}': 1197 }
+
+function findFirstIllegalCharacter(line: Token[]): Closing | void {
+  const openingCharacters: Opening[] = []
+  for (let i = 0; i < line.length; i++) {
+    if (opening.includes(line[i] as Opening)) {
+      openingCharacters.push(line[i] as Opening)
+    } else {
+      const lastOpeningChar = openingCharacters[openingCharacters.length - 1]
+      if (closing[opening.indexOf(lastOpeningChar)] == line[i]) {
+        openingCharacters.pop()
+      } else {
+        return line[i] as Closing
+      }
+    }
+  }
+}
+
 type ActionResponse = {
   syntaxErrorScore: number
 }
 
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
-  const lines = (formData.get('input') as string).split('\r\n').filter((line) => line.length)
+  const lines = (formData.get('input') as string)
+    .split('\r\n')
+    .filter((line) => line.length)
+    .map((line) => line.split('')) as Token[][]
 
-  return json({ syntaxErrorScore: -1 } as ActionResponse)
+  const illegalCharacters = lines.map(findFirstIllegalCharacter).filter(Boolean) as Closing[]
+
+  return json({ syntaxErrorScore: illegalCharacters.reduce((sum, cur) => sum + points[cur], 0) } as ActionResponse)
 }
 
 export default function () {
