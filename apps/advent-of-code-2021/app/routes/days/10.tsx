8175c1814dcacb7cfd8b4ca949b011f1528a90a5
Martin Gansler <martin.gansler@codecentric.de>
2021-12-10 19:53:59 +0100

    Day 10 - Part 2

--- apps/advent-of-code-2021/app/routes/days/10.tsx
+++ apps/advent-of-code-2021/app/routes/days/10.tsx
@@ -6,7 +6,8 @@ const opening: Opening[] = ['[', '{', '(', '<']
 const closing: Closing[] = [']', '}', ')', '>']
 type Token = Opening | Closing
 
-const points: Record<Closing, number> = { ')': 3, '>': 25137, ']': 57, '}': 1197 }
+const syntaxErrorPoints: Record<Closing, number> = { ')': 3, '>': 25137, ']': 57, '}': 1197 }
+const closingCharacterPoints: Record<Closing, number> = { ')': 1, '>': 4, ']': 2, '}': 3 }
 
 function findFirstIllegalCharacter(line: Token[]): Closing | void {
   const openingCharacters: Opening[] = []
@@ -24,8 +25,32 @@ function findFirstIllegalCharacter(line: Token[]): Closing | void {
   }
 }
 
+function getClosingCharacters(line: Token[]): Closing[] {
+  const openingCharacters: Opening[] = []
+  for (let i = 0; i < line.length; i++) {
+    if (opening.includes(line[i] as Opening)) {
+      openingCharacters.push(line[i] as Opening)
+    } else {
+      const lastOpeningChar = openingCharacters[openingCharacters.length - 1]
+      if (closing[opening.indexOf(lastOpeningChar)] == line[i]) {
+        openingCharacters.pop()
+      } else {
+        return []
+      }
+    }
+  }
+  return openingCharacters.map((o) => closing[opening.indexOf(o)]).reverse()
+}
+
+function calculateCompletionPoints(line: Closing[]): number {
+  return line.reduce((acc, cur) => {
+    return acc * 5 + closingCharacterPoints[cur]
+  }, 0)
+}
+
 type ActionResponse = {
   syntaxErrorScore: number
+  middleScore: number
 }
 
 export const action: ActionFunction = async ({ request }) => {
@@ -36,8 +61,16 @@ export const action: ActionFunction = async ({ request }) => {
     .map((line) => line.split('')) as Token[][]
 
   const illegalCharacters = lines.map(findFirstIllegalCharacter).filter(Boolean) as Closing[]
+  const closingCharacterPoints = lines
+    .map(getClosingCharacters)
+    .filter((chars) => chars.length > 0)
+    .map(calculateCompletionPoints)
+    .sort((a, b) => a - b)
 
-  return json({ syntaxErrorScore: illegalCharacters.reduce((sum, cur) => sum + points[cur], 0) } as ActionResponse)
+  return json({
+    syntaxErrorScore: illegalCharacters.reduce((sum, cur) => sum + syntaxErrorPoints[cur], 0),
+    middleScore: closingCharacterPoints[Math.floor(closingCharacterPoints.length / 2)],
+  } as ActionResponse)
 }
 
 export default function () {
@@ -52,7 +85,7 @@ export default function () {
       <br />
       <button>Solution!</button>
       {result ? <div>Solution (Part 1): {result.syntaxErrorScore}</div> : null}
-      {result ? <div>Solution (Part 2): TBD</div> : null}
+      {result ? <div>Solution (Part 2): {result.middleScore}</div> : null}
     </Form>
   )
 }
