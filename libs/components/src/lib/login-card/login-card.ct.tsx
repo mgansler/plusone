3598bf48702046e93fae3e12acf7539d2321c852
Martin Gansler <martin.gansler@codecentric.de>
2021-12-13 18:14:48 +0100

    Add ThemeProvider to component tests

--- libs/components/src/lib/login-card/login-card.ct.tsx
+++ libs/components/src/lib/login-card/login-card.ct.tsx
@@ -1,13 +1,16 @@
 import { mount } from '@cypress/react'
+import { createTheme, ThemeProvider } from '@mui/material'
 
 import { LoginCard } from './login-card'
 
 describe('LoginCard', () => {
   it('should render the child', () => {
     mount(
-      <LoginCard>
-        <div>hello world</div>
-      </LoginCard>,
+      <ThemeProvider theme={createTheme()}>
+        <LoginCard>
+          <div>hello world</div>
+        </LoginCard>
+      </ThemeProvider>,
     )
 
     cy.findByText(/hello world/i).should('be.visible')
