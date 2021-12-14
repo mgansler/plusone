02c359f450c03fd53cb848225d180aeaa836bbef
Martin Gansler <martin.gansler@codecentric.de>
2021-12-14 16:59:45 +0100

    Day 14 - Day 1

--- apps/advent-of-code-2021/app/routes/days/14.tsx
+++ apps/advent-of-code-2021/app/routes/days/14.tsx
@@ -1,5 +1,15 @@
 import { ActionFunction, Form, json, useActionData } from 'remix'
 
+function parseInstructions(raw: string): Record<string, string> {
+  return raw
+    .split('\r\n')
+    .filter((line) => line.length)
+    .reduce((instructions, line) => {
+      const [pair, insertion] = line.split(' -> ')
+      return { ...instructions, [pair]: insertion }
+    }, {})
+}
+
 type ActionResponse = {
   mostCommon: number
   leastCommon: number
@@ -8,12 +18,32 @@ type ActionResponse = {
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData()
   const [template, insertions] = (formData.get('input') as string).split('\r\n\r\n')
+  let copy = template
 
-  console.log({ template, insertions })
+  const instructions = parseInstructions(insertions)
+
+  for (let round = 0; round < 10; round++) {
+    let newTemplate = ''
+    for (let i = 0; i < copy.length - 1; i++) {
+      const pair = copy.slice(i, i + 2)
+      if (i === 0) {
+        newTemplate += pair.slice(0, 1)
+      }
+      newTemplate += instructions[pair] + pair.slice(1, 2)
+    }
+    copy = newTemplate
+  }
+
+  const quantities = copy.split('').reduce((acc: Record<string, number>, cur) => {
+    return {
+      ...acc,
+      [cur]: (acc[cur] || 0) + 1,
+    }
+  }, {})
 
   return json({
-    mostCommon: 0,
-    leastCommon: 0,
+    mostCommon: Math.max(...Object.values(quantities)),
+    leastCommon: Math.min(...Object.values(quantities)),
   } as ActionResponse)
 }
 
