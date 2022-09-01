62d4c63202339bfe4ca7405c7a0eeb14cf713984
Martin Gansler <martin.gansler@codecentric.de>
2022-09-01 11:27:13 +0200

    Update all non-major dependencies (#2137)
    * Update all non-major dependencies
    
    * Run migrations
    
    * Manually update versions
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- libs/feeds/web/shared/jest.config.ts
+++ libs/feeds/web/shared/jest.config.ts
@@ -3,7 +3,7 @@ export default {
   displayName: 'feeds-web-shared',
 
   transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/feeds/web/shared',
