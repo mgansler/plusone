3dc99dae3eae2c79a09146aeb5a696f56328f05e
Martin Gansler <martin.gansler@codecentric.de>
2022-09-14 13:26:42 +0200

    Update all non-major dependencies (#2153)
    * Update all non-major dependencies
    
    * I don't know why useMatch doesn't work anymore and at this point I am too afraid to ask
    
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- apps/advent-of-code-2021/app/routes/days.tsx
+++ apps/advent-of-code-2021/app/routes/days.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from '@remix-run/react'
-import { useMatch } from 'react-router-dom'
+import { Outlet, useMatches } from '@remix-run/react'
+import { matchPath } from 'react-router-dom'
 
 export default function () {
-  const match = useMatch('days/:day')
+  const matches = useMatches()
 
   return (
     <main>
-      <h2>Day {Number(match?.params.day)}</h2>
+      <h2>Day {Number(matchPath('/days/:day', matches[matches.length - 1].pathname)?.params.day)}</h2>
       <Outlet />
     </main>
   )
