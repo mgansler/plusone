f90c1d63677f27e0c7dad345c64e9b96ba936d4f
Martin Gansler <martin.gansler@codecentric.de>
2022-10-14 14:13:04 +0200

    Move into pages directory

--- apps/feeds/web/src/app/app.tsx
+++ apps/feeds/web/src/app/app.tsx
@@ -1,8 +1,8 @@
 import { Link, Route, Routes } from 'react-router-dom'
 
 import { Health } from './health'
-import { Login } from './login'
-import { Register } from './login/register'
+import { Login } from './pages/login'
+import { Register } from './pages/login/register'
 
 export function App() {
   return (
