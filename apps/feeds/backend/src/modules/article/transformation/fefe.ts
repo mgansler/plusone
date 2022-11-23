3eba511f43581f13ae326aac82341442e13c9f93
Martin Gansler <martin.gansler@codecentric.de>
2022-11-23 21:42:12 +0100

    Add support for Fefes Blog

---
+++ apps/feeds/backend/src/modules/article/transformation/fefe.ts
@@ -0,0 +1,12 @@
+import { ArticleBuilderFn } from './transformation'
+
+export const fefeArticleBuilder: ArticleBuilderFn = async (item) => {
+  return {
+    content: item.content,
+    contentBody: item.content,
+    guid: item.guid ?? item.id,
+    link: item.link,
+    title: item.title,
+    date: new Date(),
+  }
+}
