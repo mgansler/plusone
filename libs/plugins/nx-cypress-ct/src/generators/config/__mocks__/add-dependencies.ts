24f823a1e2508b7e656d5db8249d5e0c1ff9d502
Martin Gansler <martin.gansler@codecentric.de>
2022-11-23 10:50:32 +0100

    Remove deprecated plugin

--- libs/plugins/nx-cypress-ct/src/generators/config/__mocks__/add-dependencies.ts
+++
@@ -1,5 +0,0 @@
-import type { Tree } from '@nrwl/devkit'
-
-export function addDependencies(tree: Tree) {
-  // we do not want to install dependencies in unit tests
-}
--- libs/plugins/nx-cypress-ct/src/generators/config/schema.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ConfigSchema {
-  projectName: string
-  video: boolean
-}
