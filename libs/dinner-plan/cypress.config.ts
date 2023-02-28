41b9331316fd97213dfa11043cc0c89c6e9b6d19
Martin Gansler <martin.gansler@codecentric.de>
2023-02-28 08:48:10 +0100

    Allow retries for flaky tests

--- libs/dinner-plan/cypress.config.ts
+++ libs/dinner-plan/cypress.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'cypress'
 import { nxComponentTestingPreset } from '@nrwl/react/plugins/component-testing'
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  component: nxComponentTestingPreset(__filename, {
-    bundler: 'webpack',
-  }) as Cypress.ConfigOptions['component'],
+  component: {
+    ...nxComponentTestingPreset(__filename, {
+      bundler: 'webpack',
+    }),
+    retries: 3,
+  },
 })
