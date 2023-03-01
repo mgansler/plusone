b7d1031d94042ccefb7e32d2794bd660480b3fd8
Martin Gansler <martin.gansler@codecentric.de>
2023-03-01 07:32:56 +0100

    Run migrations

--- apps/plugins/nx-remix-e2e/jest.config.ts
+++ apps/plugins/nx-remix-e2e/jest.config.ts
@@ -1,14 +1,9 @@
 module.exports = {
   displayName: 'plugins-nx-remix-e2e',
   preset: '../../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/plugins/nx-remix-e2e',
 }
