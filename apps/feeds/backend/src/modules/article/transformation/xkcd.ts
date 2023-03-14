68782f19f29757030009e5fc82fe1a2492fc4f81
Martin Gansler <martin.gansler@codecentric.de>
2023-03-14 07:51:13 +0100

    Fix and use xkcd in e2e test

--- apps/feeds/backend/src/modules/article/transformation/xkcd.ts
+++ apps/feeds/backend/src/modules/article/transformation/xkcd.ts
@@ -3,7 +3,7 @@ import { ArticleBuilderFn } from './transformation'
 export const xkcdArticleBuilder: ArticleBuilderFn = async (item) => {
   return {
     content: item.summary,
-    guid: item.id,
+    guid: item.guid,
     link: item.link,
     title: item.title,
     date: new Date(item.isoDate),
