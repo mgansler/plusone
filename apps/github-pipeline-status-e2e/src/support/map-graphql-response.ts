c75cfddf50e0ac12e02f2fe439a1821da26c89de
Martin Gansler <martin.gansler@codecentric.de>
2023-09-07 14:40:02 +0200

    Add temporary throw statement

--- apps/github-pipeline-status-e2e/src/support/map-graphql-response.ts
+++ apps/github-pipeline-status-e2e/src/support/map-graphql-response.ts
@@ -2,6 +2,9 @@ import type { RouteHandlerController } from 'cypress/types/net-stubbing'
 
 export function mapGraphqlResponse(req: Parameters<RouteHandlerController>[0], mapping: Record<string, unknown>) {
   for (const queryString in mapping) {
+    if (!req.body.query) {
+      throw new Error(JSON.stringify(req, null, 2))
+    }
     if (
       req.body.query.includes(queryString) ||
       (req.body.variables?.queryString && req.body.variables.queryString.includes(queryString))
