8e43b8f2f5bc1ff28e4fe380e2f6f6818d04d95d
Martin Gansler <martin.gansler@codecentric.de>
2023-09-14 16:20:41 +0200

    Remove not working e2e tests

--- apps/elgato/backend-e2e/src/elgato-backend/elgato-backend.spec.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-describe('GET /api', () => {
-  it('should return 200 for index', async () => {
-    const res = await axios.get(`/api`)
-
-    expect(res.status).toBe(200)
-  })
-})
