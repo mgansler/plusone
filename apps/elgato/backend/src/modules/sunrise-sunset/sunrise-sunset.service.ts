f93dd8cdaa6cf18194c959cd2cdd1db075992226
Martin Gansler <martin.gansler@codecentric.de>
2023-10-20 19:36:53 +0200

    Don't fail app when there is no location saved

--- apps/elgato/backend/src/modules/sunrise-sunset/sunrise-sunset.service.ts
+++ apps/elgato/backend/src/modules/sunrise-sunset/sunrise-sunset.service.ts
@@ -30,47 +30,52 @@ export class SunriseSunsetService implements OnModuleInit {
   @Cron(CronExpression.EVERY_DAY_AT_3AM)
   async setSunriseTimings() {
     // We want to start/end the sequence at dawn/dusk
-    const sunriseSunsetTimes = await this.locationService.getLocationData()
-    const devices = await this.prismaService.device.findMany({ where: { sunrise: true } })
+    try {
+      const sunriseSunsetTimes = await this.locationService.getLocationData()
 
-    const sunriseSequenceTotalDuration = getTotalDurationInMs(sunrise)
+      const devices = await this.prismaService.device.findMany({ where: { sunrise: true } })
 
-    // Only create a job if dawn is after now
-    if (sunriseSunsetTimes.dawn > new Date()) {
-      // Clear existing jobs/timeouts from yesterday or after service restart
-      if (this.schedulerRegistry.doesExist('cron', 'sunrise')) {
-        this.logger.debug('Clearing cron job: sunrise')
-        this.schedulerRegistry.deleteCronJob('sunrise')
+      const sunriseSequenceTotalDuration = getTotalDurationInMs(sunrise)
+
+      // Only create a job if dawn is after now
+      if (sunriseSunsetTimes.dawn > new Date()) {
+        // Clear existing jobs/timeouts from yesterday or after service restart
+        if (this.schedulerRegistry.doesExist('cron', 'sunrise')) {
+          this.logger.debug('Clearing cron job: sunrise')
+          this.schedulerRegistry.deleteCronJob('sunrise')
+        }
+        if (this.schedulerRegistry.doesExist('timeout', 'sunrise')) {
+          this.logger.debug('Clearing timeout: sunrise')
+          clearTimeout(this.schedulerRegistry.getTimeout('sunrise'))
+        }
+
+        const sunriseJob = new CronJob({
+          cronTime: sunriseSunsetTimes.dawn,
+          onTick: () => {
+            console.log(
+              'Starting sunrise...',
+              devices.map((d) => d.name),
+            )
+            devices.forEach((d) => this.elgatoService.setLightStripState(d, sunrise))
+
+            const turnOffCallback = () => {
+              this.logger.log(`Turning off ${devices.length} devices after sunrise`)
+              devices.forEach((device) => this.elgatoService.setDevicePowerState(device, DevicePowerState.off))
+            }
+
+            // The last scene (brightness: 0) has a duration of 60s. Turn the device off during that duration.
+            this.schedulerRegistry.addTimeout(
+              'sunrise',
+              setTimeout(turnOffCallback, sunriseSequenceTotalDuration - 30_000),
+            )
+          },
+        })
+
+        this.schedulerRegistry.addCronJob('sunrise', sunriseJob)
+        sunriseJob.start()
       }
-      if (this.schedulerRegistry.doesExist('timeout', 'sunrise')) {
-        this.logger.debug('Clearing timeout: sunrise')
-        clearTimeout(this.schedulerRegistry.getTimeout('sunrise'))
-      }
-
-      const sunriseJob = new CronJob({
-        cronTime: sunriseSunsetTimes.dawn,
-        onTick: () => {
-          console.log(
-            'Starting sunrise...',
-            devices.map((d) => d.name),
-          )
-          devices.forEach((d) => this.elgatoService.setLightStripState(d, sunrise))
-
-          const turnOffCallback = () => {
-            this.logger.log(`Turning off ${devices.length} devices after sunrise`)
-            devices.forEach((device) => this.elgatoService.setDevicePowerState(device, DevicePowerState.off))
-          }
-
-          // The last scene (brightness: 0) has a duration of 60s. Turn the device off during that duration.
-          this.schedulerRegistry.addTimeout(
-            'sunrise',
-            setTimeout(turnOffCallback, sunriseSequenceTotalDuration - 30_000),
-          )
-        },
-      })
-
-      this.schedulerRegistry.addCronJob('sunrise', sunriseJob)
-      sunriseJob.start()
+    } catch (e) {
+      this.logger.error(e)
     }
   }
 }
