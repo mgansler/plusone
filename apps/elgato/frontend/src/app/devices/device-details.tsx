a9bcb8443c137c7330a2e8e62ade019329dceccb
Martin Gansler <martin.gansler@codecentric.de>
2023-11-27 17:59:42 +0100

    Throttle calls in frontend

--- apps/elgato/frontend/src/app/devices/device-details.tsx
+++ apps/elgato/frontend/src/app/devices/device-details.tsx
@@ -1,6 +1,8 @@
 import { useParams } from '@remix-run/react'
 import { useQueryClient } from '@tanstack/react-query'
 import { convert } from 'colvertize'
+import { throttle } from 'lodash'
+import { useCallback } from 'react'
 
 import { DeviceType, useTransitionToColor, useValidatedDeviceDetails } from '@plusone/elgato-api-client'
 
@@ -16,13 +18,15 @@ export function DeviceDetails() {
   const { mutate } = useTransitionToColor({
     mutation: { onSuccess: () => queryClient.invalidateQueries({ queryKey }) },
   })
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const transitionToColor = useCallback(throttle(mutate, 1200), [])
 
   const setStripColor = (color: string) => {
     const { h, s, l } = convert(color, 'hsl')
     const hue = Math.round(h)
     const saturation = Math.round(s * 100)
     const brightness = Math.round(l * 100)
-    mutate({ id: deviceId, data: { hue, saturation, brightness } })
+    transitionToColor({ id: deviceId, data: { hue, saturation, brightness } })
   }
 
   if (isLoading) {
