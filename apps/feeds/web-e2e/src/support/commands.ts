5fed2bb0852f404f1defcc34dd9398c79ed59fea
Martin Gansler <martin.gansler@codecentric.de>
2023-12-01 12:26:37 +0100

    Migrate pipeline to affected based approach

--- apps/feeds/web-e2e/src/support/commands.ts
+++ apps/feeds/web-e2e/src/support/commands.ts
@@ -15,7 +15,10 @@ declare namespace Cypress {
 }
 
 Cypress.Commands.add('forceFetchFeeds', () => {
-  cy.request('POST', '/api/authentication/login', { username: 'root', password: 'keep_this_secret' }).then((resp) => {
+  cy.request('POST', '/api/authentication/login', {
+    username: 'root',
+    password: 'local_admin_password',
+  }).then((resp) => {
     cy.request({ method: 'POST', url: '/api/scheduling/now', auth: { bearer: resp.body.access_token } })
   })
 })
@@ -53,7 +56,7 @@ Cypress.Commands.add('loginFreshUser', () => {
 Cypress.Commands.add('loginAdmin', () => {
   cy.visit('/login')
   cy.findByRole('textbox', { name: /username/i }).type('root')
-  cy.findByLabelText(/password/i).type('keep_this_secret')
+  cy.findByLabelText(/password/i).type('local_admin_password')
   cy.findByRole('button', { name: /login/i }).click()
   cy.location('pathname', { timeout: 10_000 }).should('eq', '/admin')
 })
---
+++ tools/scripts/install-docker-debian.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+apt-get update
+apt-get install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+apt-get install -y docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
