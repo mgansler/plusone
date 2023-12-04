3d7719c6bd6e91a4b7c59cbf4c5db4039c7e0580
Martin Gansler <martin.gansler@codecentric.de>
2023-12-04 11:18:21 +0100

    Deploy migrations on serve

---
+++ libs/plugins/nx-prisma/src/executors/deploy/executor.ts
@@ -0,0 +1,28 @@
+import { spawn } from 'child_process'
+import { join } from 'path'
+
+import type { ExecutorContext } from '@nx/devkit'
+
+import type { FormatExecutorSchema } from './schema'
+
+export default async function runExecutor(
+  options: FormatExecutorSchema,
+  context: ExecutorContext,
+): Promise<{ success: boolean }> {
+  if (!context.projectName) {
+    return { success: false }
+  }
+
+  const schemaPath = join(context.workspace.projects[context.projectName].sourceRoot, options.schema)
+  const args: string[] = ['prisma', 'migrate', 'deploy', '--schema', schemaPath]
+  const prismaDeploy = spawn('yarn', args)
+
+  return new Promise((resolve) => {
+    prismaDeploy.stdout.on('data', (data) => console.log(data.toString()))
+    prismaDeploy.stderr.on('data', (data) => console.error(data.toString()))
+
+    prismaDeploy.on('close', (code) => {
+      resolve({ success: code === 0 })
+    })
+  })
+}
