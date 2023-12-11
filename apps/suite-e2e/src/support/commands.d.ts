790a573e2c0e6735e9479683e1ff64b55f2bda0b
Martin Gansler <martin.gansler@codecentric.de>
2023-12-11 12:10:06 +0100

    Unify cypress commands definition

--- apps/suite-e2e/src/support/commands.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Cypress {
-  interface Chainable {
-    findDropzoneByName(name: string): Chainable<Element>
-
-    drag(source: string, target: string): Chainable<Element>
-  }
-}
--- apps/suite-e2e/src/support/commands.ts
+++ apps/suite-e2e/src/support/commands.ts
@@ -1,5 +1,17 @@
-Cypress.Commands.add('findDropzoneByName', (name) => {
-  return cy.findByText(name).parent().parent().parent().children().last()
+import Chainable = Cypress.Chainable
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Chainable<Subject> {
+    findDropzoneByName(name: string): Chainable<Element>
+
+    drag(text: string, target: string): void
+  }
+}
+
+Cypress.Commands.add('findDropzoneByName', (name: string) => {
+  return cy.findByText(name).parent().parent().parent().children().last() as unknown as Chainable<Element>
 })
 
 Cypress.Commands.add('drag', { prevSubject: false }, (source: string, target: string) => {
