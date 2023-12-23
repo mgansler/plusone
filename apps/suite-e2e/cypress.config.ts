6b5d60438319dbba056f910b8659589208afcee5
Martin Gansler <martin.gansler@codecentric.de>
2023-12-23 12:29:27 +0100

    Add junit reporting for e2e tests

--- apps/suite-e2e/cypress.config.ts
+++ apps/suite-e2e/cypress.config.ts
@@ -2,6 +2,10 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: '../../reports/e2e/suite.xml',
+  },
   e2e: {
     ...nxE2EPreset(__dirname),
     experimentalRunAllSpecs: true,
