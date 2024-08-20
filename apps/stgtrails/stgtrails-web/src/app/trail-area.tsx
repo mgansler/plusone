45b9d03acd6182e24ba3d02a64639e011c8eb0a3
Martin Gansler <martin.gansler@codecentric.de>
2024-08-20 10:15:29 +0200

    Improve diagram labels

--- apps/stgtrails/stgtrails-web/src/app/trail-area.tsx
+++ apps/stgtrails/stgtrails-web/src/app/trail-area.tsx
@@ -1,5 +1,6 @@
-import type { ChartData } from 'chart.js'
+import type { ChartData, ChartType } from 'chart.js'
 import {
+  BarElement,
   CategoryScale,
   Chart as ChartJS,
   Legend,
@@ -9,15 +10,34 @@ import {
   Title,
   Tooltip,
 } from 'chart.js'
-import { Line } from 'react-chartjs-2'
+import type { Plugin } from 'chart.js/dist/types'
+import { Chart } from 'react-chartjs-2'
 
 import { useValidatedTrailsForTrailArea, useValidatedWeatherDataForTrailArea } from '@plusone/stgtrails-api-client'
 
-const items = [CategoryScale, LinearScale, Title, Legend, Tooltip, PointElement, LineElement]
+const items = [CategoryScale, LinearScale, Title, Legend, Tooltip, PointElement, LineElement, BarElement]
 ChartJS.register(items)
 
 type TrailAreaProps = {
   trailAreaId: number
+}
+
+const nowPlugin: Plugin = {
+  id: 'now',
+  beforeDraw(chart: ChartJS<ChartType>) {
+    const ctx = chart.ctx
+
+    const now = new Date()
+    const xValue = chart.scales.x.getPixelForValue(now.getUTCHours() + 24 + now.getMinutes() / 60)
+    ctx.save()
+    ctx.strokeStyle = 'blue'
+    ctx.lineWidth = 2
+    ctx.beginPath()
+    ctx.moveTo(xValue, 30)
+    ctx.lineTo(xValue, chart.height - 105)
+    ctx.stroke()
+    ctx.restore()
+  },
 }
 
 export function TrailArea({ trailAreaId }: TrailAreaProps) {
@@ -28,21 +48,29 @@ export function TrailArea({ trailAreaId }: TrailAreaProps) {
     return null
   }
 
-  const chartData: ChartData<'line'> = {
+  const chartData: ChartData = {
     labels: weather.map((v) => {
       const ts = new Date(v.time)
-      return ts.toLocaleString()
+      return ts.getHours() > 0 && ts.getHours() < 3 ? ts.toLocaleString() : ts.toLocaleTimeString()
     }),
     datasets: [
-      { label: 'Rain', data: weather.map((v) => v.rain), backgroundColor: 'blue' },
-      { label: 'Soil Moisture 0 to 1cm', data: weather.map((v) => v.soilMoisture0To1cm), backgroundColor: 'red' },
+      {
+        label: 'Soil Moisture 0 to 1cm',
+        type: 'line',
+        data: weather.map((v) => v.soilMoisture0To1cm),
+        backgroundColor: 'blue',
+        pointRadius: 1.5,
+      },
       {
         label: 'Moisture Threshold (0.3)',
+        type: 'line',
         data: weather.map(() => 0.3),
-        backgroundColor: 'purple',
-        borderColor: 'purple',
+        backgroundColor: 'red',
+        borderColor: 'red',
         pointRadius: 0,
+        borderWidth: 2,
       },
+      { label: 'Rain (mm)', type: 'bar', data: weather.map((v) => v.rain), backgroundColor: 'lightblue' },
     ],
   }
 
@@ -50,7 +78,14 @@ export function TrailArea({ trailAreaId }: TrailAreaProps) {
     <div>
       <ul>{trails?.map((trail) => <li key={trail.id}>{trail.name}</li>)}</ul>
       <div style={{ maxHeight: 600 }}>
-        <Line data={chartData} options={{ responsive: true }} />
+        <Chart
+          type={'bar'}
+          data={chartData}
+          plugins={[nowPlugin]}
+          options={{
+            responsive: true,
+          }}
+        />
       </div>
     </div>
   )
