10b766027bacaf70f2e9a86da57256a7ffe97cb9
Martin Gansler <martin.gansler@codecentric.de>
2024-11-25 09:30:26 +0100

    Revert unfinished request-logger changes

--- libs/nestjs-services/src/request-logger-middleware/request-logger-middleware.ts
+++ libs/nestjs-services/src/request-logger-middleware/request-logger-middleware.ts
@@ -3,17 +3,11 @@ import { NextFunction, Request, Response } from 'express'
 
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
-  private logger = new Logger('', { timestamp: true })
+  private logger = new Logger()
 
   use(req: Request, res: Response, next: NextFunction) {
-    const startTs = Date.now()
-
-    // @ts-expect-error private method
-    this.logger.localInstance.updateAndGetTimestampDiff()
-
     res.on('close', () => {
       const { statusCode } = res
-      process.stdout.write(`[Nest] ${process.pid}`)
       this.logger.log(req.originalUrl.replace('/api', ''), `${req.method} ${statusCode}`)
     })
 
