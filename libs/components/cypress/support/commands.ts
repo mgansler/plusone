0a429e256a5a4b94c606d16f2ad05a3d58b42b0c
Martin Gansler <martin@martingansler.de>
2025-01-17 08:45:19 +0100

    Update dependency cypress to v14

--- libs/components/cypress/support/commands.ts
+++ libs/components/cypress/support/commands.ts
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
-import { mount } from 'cypress/react18'
+import { mount } from 'cypress/react'
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
