8954286e36db5b65f16d6f5aeb900806394b076c
Martin Gansler <martin@martingansler.de>
2025-03-07 07:45:40 +0100

    Update all non-major dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ module.exports = [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/vite.config.*.timestamp*', '**/vitest.config.*.timestamp*'],
   },
   {
     plugins: {
