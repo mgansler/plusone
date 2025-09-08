ab3baf8e17e121763ce3d310a209d2ec2a099adc
Martin Gansler <martin.gansler@codecentric.de>
2025-09-08 22:30:21 +0200

    Exclude ESM only package uuid from jest transformation

--- apps/stgtrails/stgtrails-backend/jest.config.ts
+++ apps/stgtrails/stgtrails-backend/jest.config.ts
@@ -2,6 +2,7 @@ export default {
   displayName: 'stgtrails-backend',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
+  transformIgnorePatterns: ['node_modules/(?!(uuid))'],
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
