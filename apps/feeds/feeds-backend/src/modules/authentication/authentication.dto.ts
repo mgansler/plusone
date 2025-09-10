51165682491fe4a58ffd04450128a94a09dae5ca
Martin Gansler <martin.gansler@codecentric.de>
2025-09-10 17:36:41 +0200

    Feeds: user uuid v7 for ids in database

--- apps/feeds/feeds-backend/src/modules/authentication/authentication.dto.ts
+++ apps/feeds/feeds-backend/src/modules/authentication/authentication.dto.ts
@@ -1,4 +1,4 @@
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty } from 'class-validator'
 
 export class LoginResponseDto {
@@ -30,8 +30,8 @@ export class UserRegistrationDto {
 }
 
 export class UserResponseDto {
-  @ApiPropertyOptional({ type: String })
-  email?: string | null
+  @ApiProperty({ type: String, nullable: true })
+  email: string | null
 
   @ApiProperty()
   id: string
