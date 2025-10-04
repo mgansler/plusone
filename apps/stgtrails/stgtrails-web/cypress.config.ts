76c9e1827a458dd5cc4b556566c9d4519bb83459
Martin Gansler <martin@martingansler.de>
2025-10-04 23:48:14 +0200

    Update all non-major dependencies

--- apps/stgtrails/stgtrails-web/cypress.config.ts
+++ apps/stgtrails/stgtrails-web/cypress.config.ts
@@ -2,5 +2,9 @@ import { nxComponentTestingPreset } from '@nx/react/plugins/component-testing'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: '../../../reports/component-test/stgtrails-web-[hash].xml',
+  },
   component: nxComponentTestingPreset(__filename, { bundler: 'vite' }),
 })
